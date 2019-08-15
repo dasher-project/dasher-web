84cdf14bbf13faf12d3e974860dbbe26cdc4dd5a
Jim Hawkins <jhawkins@hands.com>
2019-08-15 21:49:31 +0100

    Added pointer tracking.

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ class Index {
         this._characters = "abcdefghijklmnopqrstuvwxyz".split("");
     }
 
-    _create(tag, attributes, text, parent) {
-        const element = document.createElementNS(
-            'http://www.w3.org/2000/svg', tag);
+    _create(
+        tag, attributes, text, parent, nameSpace='http://www.w3.org/2000/svg'
+    ) {
+        const element = document.createElementNS(nameSpace, tag);
         if (attributes !== undefined) {
             for (const [key, value] of Object.entries(attributes)) {
                 element.setAttribute(key, value);
@@ -22,23 +23,80 @@ class Index {
         (parent === undefined ? this._svg : parent).appendChild(element);
         return element;
     }
+    _createHTML(tag, attributes, text, parent) {
+        return this._create(
+            tag, attributes, text,
+            parent === undefined ? this._parent : parent,
+            'http://www.w3.org/1999/xhtml'
+        );
+    }
 
     load() {
-        const height = 30;
+        const div = this._createHTML('div');
+        const xTextNode = this._createHTML('span', {}, "X", div).firstChild;
+        this._createHTML('span', {}, ",", div);
+        const yTextNode = this._createHTML('span', {}, "Y", div).firstChild;
+        const setXY = (x, y) => {
+            xTextNode.nodeValue = x.toFixed().toString();
+            yTextNode.nodeValue = y.toFixed().toString();
+        }
+
+        const rectHeight = 30;
+        const svgHeight = rectHeight * this._characters.length;
+        this._createHTML('span', {}, ` ${svgHeight}`, div);
         this._svg = this._create(
-            'svg', {"height":height * this._characters.length}, undefined, 
-            this._parent
+            'svg', {
+                viewBox:
+                    `${svgHeight * -0.5} ${svgHeight * -0.5}` +
+                    ` ${svgHeight} ${svgHeight}`,
+                height:svgHeight}, undefined, this._parent
         );
-        let yPosition = 5;
+        this._create('line', {
+            x1:"0", y1:"-50%", x2:"0", y2:"50%",
+            stroke:"black", "stroke-width":"1px"
+        });
+        this._create('line', {
+            x1:"-50%", y1:"0", x2:"50%", y2:"0",
+            stroke:"black", "stroke-width":"1px"
+        });
+
+        // Next bit of code is in a timeout to ensure that the browswer can
+        // render the svg before it is called.
+        setTimeout(() => {
+            const bbox = this._svg.getBoundingClientRect();
+            this._createHTML(
+                'span', {},
+                ` bbox:(${bbox.x}, ${bbox.y}, ${bbox.width}, ${bbox.height})`,
+                div
+            );
+
+            const pointerLine = this._create('line', {
+                x1:"0", y1:"0", x2:"0", y2:"0",
+                stroke:"red", "stroke-width":"1px"
+            });
+    
+            const xAdjust = -1 * (bbox.x + (bbox.width * 0.5));
+            const yAdjust = bbox.y + (bbox.height * 0.5);
+            this._svg.addEventListener('pointermove', (event) => {
+                const x = xAdjust + event.clientX;
+                const y = yAdjust - event.clientY;
+                pointerLine.setAttribute('x2', x.toString());
+                pointerLine.setAttribute('y2', (-1 * y).toString());
+                setXY(x, y);
+            }, {capture:true});
+        
+        }, 0);
+
+        let yPosition = -0.5 * svgHeight;
         for(const [index, character] of this._characters.entries()) {
             this._create('rect', {
-                x:5, y:yPosition, width:height, height:height,
+                x:5, y:yPosition, width:rectHeight, height:rectHeight,
                 fill:(index % 2 === 0 ? "lightgray" : "lightblue")
             });
             this._create('text', {
-                x:10, y:yPosition + height / 2.0, fill:"black"
+                x:10, y:yPosition + rectHeight / 2.0, fill:"black"
             }, character );
-            yPosition += height;
+            yPosition += rectHeight;
         }
 
     }
