dd5b6f0dcbad252dcfe2776e18f10f4495cf9e81
Jeremy <j.cope@me.com>
2020-08-11 18:11:43 -0400

    Additional test- take a screenshot.

--- tests/dasher.test.js
+++ tests/dasher.test.js
@@ -8,3 +8,13 @@ describe('Dasher', () => {
     await expect(page).toMatch('dasher')
   })
 })
+
+describe('Dasher Plus', () => {
+  beforeAll(async () => {
+    await page.goto('http://127.0.0.1:8000')
+  })
+
+  it('A screenshot will be saved.', async () => {
+    await page.screenshot({path: 'screenshot.png'});
+  })
+})
