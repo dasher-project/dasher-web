d022c763fe8504902c7214f18b55d5d53db14b5e
Jeremy <j.cope@me.com>
2021-09-01 10:40:24 -0400

    Update localhost

--- tests/dasher.test.js
+++ tests/dasher.test.js
@@ -1,7 +1,7 @@
 
 describe('Dasher', () => {
   beforeAll(async () => {
-    //await page.goto('http://127.0.0.1:8000')
+    await page.goto('localhost:8000')
   })
 
   it('should display "dasher" text on page', async () => {
@@ -11,7 +11,7 @@ describe('Dasher', () => {
 
 describe('Dasher Plus', () => {
   beforeAll(async () => {
-    //await page.goto('http://127.0.0.1:8000')
+    await page.goto('localhost:8000')
   })
 
   it('A screenshot will be saved.', async () => {
