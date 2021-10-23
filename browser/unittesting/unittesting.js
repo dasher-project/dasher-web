3e5becb10f60428a4c8f893d55554cd3fe7d657a
Jim Hawkins <sjjhsjjhsjjh@gmail.com>
2021-10-23 16:55:35 +0100

    WiP commit for safekeeping with TestAssertion as an Error subclass and without rubbish assertion popping code
    Signed-off-by: Jim Hawkins <sjjhsjjhsjjh@gmail.com>

--- browser/unittesting/unittesting.js
+++ browser/unittesting/unittesting.js
@@ -16,6 +16,8 @@
 //     to the test function. TestResult has methods by which the test function
 //     can assert conditions as it runs.
 // -   TestAssertion records the results of a single assertion within a test.
+//     TestAssertion is an Error subclass so that it can be thrown and so that
+//     it can store a stack trace.
 //
 // By default, TestResult throws an error that includes a stack trace when an
 // assertion fails. Chrome developer tools, for example, facilitate navigating
@@ -24,30 +26,137 @@
 // TOTH the exemplary Python unittest module, see
 // https://docs.python.org/library/unittest.html
 
-export class TestFailedError extends Error {
-    // constructor(...messages) {
-    //     super();
-    // }
+const defaultErrorMessageJoiner = "\n";
 
-    // get messages() {}
-}
+export class TestAssertion extends Error {
+    constructor(passed, messages, ...parameters) {
+        if (messages === undefined) {
+            super(...parameters);
+            this._messages = undefined;
+        }
+        else {
+            super(messages.join(defaultErrorMessageJoiner), ...parameters);
+            this._messages = messages.slice();
+        }
 
-export class TestAssertion {
-    constructor(passed, messages, error) {
         this._passed = passed;
-        this._messages = messages;
-        this._error = error;
+
+        // Default values.
+        // Non-default values haven't been tested, oh the irony.
+        this._errorMessageJoiner = "\n";
     }
 
     get passed() {return this._passed;}
+    set passed(passed) {this._passed = passed;}
     get messages() {return this._messages;}
-    get error() {return this._error;}
+    set messages(messages) {
+        this._messages = messages.slice();
+        this.message = this._messages.join(this._errorMessageJoiner);
+    }
 
-    joinedMessages(joiner) {
-        return (
-            this._messages === undefined ? undefined
-            : this._messages.join(joiner)
-        );
+    assertTypeEqual(left, right, ...messages) {
+        const leftType = typeof(left);
+        const rightType = typeof(right);
+        this.passed = (leftType === rightType);
+        this.messages = [...messages
+            , `assertTypeEqual(${left},${right},...)` +
+            ` ${leftType} === ${rightType}.`
+        ];
+        return this;
+    }
+
+    assertInstanceOf(left, right, ...messages) {
+        this.passed = (left instanceof right);
+        this.messages = [
+            ...messages,
+            `assertInstanceOf(${left},${right},...)` +
+            ` ${left} instanceof ${right.name}.`
+        ];
+        return this;
+    }
+
+    assertTrue(value, ...messages) {
+        this.passed = (value === true);
+        this.messages = [...messages, `assertTrue(${value},...)`];
+        return this;
+    }
+
+    assertFalse(value, ...messages) {
+        this.passed = (value === false);
+        this.messages = [...messages, `assertFalse(${value},...)`];
+        return this;
+    }
+
+    assertUndefined(value, ...messages) {
+        this.passed = (value === undefined);
+        this.messages = [...messages, `assertUndefined(${value},...)`];
+        return this;
+    }
+
+    assertNotUndefined(value, ...messages) {
+        this.passed = (value !== undefined);
+        this.messages = [...messages, `assertNotUndefined(${value},...)`];
+        return this;
+    }
+
+    assertEqual(left, right, ...messages) {
+        this.passed = (left === right);
+        this.messages = [...messages, `assertEqual(${left},${right},...).`];
+        return this;
+    }
+
+    assertNotEqual(left, right, ...messages) {
+        this.assertTypeEqual(left, right, ...messages,
+            `Same types in assertNotEqual(${left},${right},...).`);
+        if (!this.passed) return this;
+
+        this.passed = (left !== right);
+        this.messages = [...messages, `assertNotEqual(${left},${right},...).`];
+        return this;
+    }
+
+    assertThrows(function_, errorType, ...messages) {
+        let return_;
+        let caught;
+        try {
+            return_ = function_();
+            caught = undefined;
+        }
+        catch (error) {
+            caught = error;
+        }
+        this.assertNotUndefined(caught, ...messages, "Threw an error.");
+        if ((!this.passed) || (errorType === undefined)) {
+            return [this, return_];
+        }
+
+        return [this.assertInstanceOf(
+            caught, errorType, ...messages, "Threw expected error type."
+        ), return_];
+    }
+
+    assertEqualArrays(left, right, ...messages) {
+        this.assertEqual(left.length, right.length, ...messages,
+            "assertEqualArrays same length.");
+        if (!this.passed) {
+            return this;
+        }
+
+        for(let index=0; index < left.length; index++) {
+            this.assertEqual(
+                left[index], right[index],
+                ...messages, `assertEqualArrays [${index}].`);
+            if (!this.passed) return this;
+        }
+
+        this.messages = [
+            ...messages, `assertEqualArrays(${left},${right},...).`
+        ];
+        return this;
+
+        // Zero-length arrays will be handled OK. The assertion of same length
+        // will pass; the for loop will be skipped leaving the passed result
+        // intact.
     }
 }
 
@@ -80,135 +189,101 @@ export class TestResult {
         this._errorMessageJoiner = errorMessageJoiner;
     }
 
-    get lastError() {
+    get lastFail() {
         for (let index = this._assertions.length - 1; index >= 0; index--) {
             const assertion = this._assertions[index];
-            if (assertion.error !== undefined) {
-                return assertion.error;
-            }
+            if (!assertion.passed) return assertion;
         }
         return undefined;
     }
 
-    _popAssertion() {
-        const lastAssertion = this._assertions.pop();
-        if (lastAssertion.passed) {
-            return true;
-        }
-        this._assertions.push(lastAssertion);
-        return false;
-    }
-
     // Rules for assert...() methods are as follows.
     //
-    // -   Must call assertTruthy() at least once.
-    // -   Must only add one assertion. If calling another assert...() method,
-    //     must remove the assertions for any assertions that pass except one.
-    //     The _popAssertion() method facilitates that.
+    // -   Must create a new TestAssertion instance, in order to generate a
+    //     stack trace even if the assertion passes. The new TestAssertion()
+    //     call should be on the best line to which navigation should be
+    //     facilitated.
+    // -   Must call an assert...() method on the new instance.
+    // -   Must push exactly one TestAssertion instance onto the _assertions
+    //     array.
+    // -   Must throw a TestAssertion if an assertion fails.
+    //
+    // The push and throw requirement can be met by calling
+    // this._resultAssertion() for example.
 
-    assertTruthy(value, ...messages) {
-        if (!!value) {
-            this._assertions.push(new TestAssertion(
-                true, messages.length === 0 ? undefined : messages));
-            return value;
+    _resultAssertion(assertion) {
+        this._assertions.push(assertion);
+        if (this.throwOnFail && !assertion.passed) {
+            throw assertion;
         }
-    
-        // Create an Error instance to get a stack trace.
-        const error = new TestFailedError(
-            messages.join(this.errorMessageJoiner));
-        this._assertions.push(new TestAssertion(false, messages, error));
-
-        if (this._throwOnFail) {
-            throw error;
-        }
-        return value;
+        return assertion;
     }
-    
+
     assertTypeEqual(left, right, ...messages) {
-        const leftType = typeof(left);
-        const rightType = typeof(right);
-        this.assertTruthy(leftType === rightType, ...messages,
-            `assertTypeEqual(${left},${right},...)` +
-            ` ${leftType} === ${rightType}.`);
+        this._resultAssertion(
+            new TestAssertion().assertTypeEqual(left, right, ...messages));
         return [left, right];
     }
 
     assertInstanceOf(left, right, ...messages) {
-        this.assertTruthy(left instanceof right, ...messages,
-            `assertInstanceOf(${left},${right},...)` +
-            ` ${left} instanceof ${right.name}.`);
+        this._resultAssertion(
+            new TestAssertion().assertInstanceOf(left, right, ...messages));
         return [left, right];
     }
     
     assertEqual(left, right, ...messages) {
-        this.assertTruthy(left === right, ...messages,
-            `assertEqual(${left},${right},...).`);
+        this._resultAssertion(
+            new TestAssertion().assertEqual(left, right, ...messages));
         return [left, right];
-    }
-    
-    assertNotEqual(left, right, ...messages) {
-        this.assertTypeEqual(left, right, ...messages,
-            `Same types in assertNotEqual(${left},${right},...).`);
-        if (this._popAssertion()) {
-            this.assertTruthy(left !== right, ...messages,
-                `assertNotEqual(${left},${right},...).`);
-        }
-        return [left, right];
-    }
-    
-    assertFalse(value, ...messages) {
-        return this.assertEqual(value, false, ...messages)[0];
-    }
-    
-    assertTrue(value, ...messages) {
-        return this.assertEqual(value, true, ...messages)[0];
-    }
-    
-    assertUndefined(value, ...messages) {
-        return this.assertEqual(value, undefined, ...messages,
-            `assertUndefined(${value})`)[0];
-    }
-    
-    assertNotUndefined(value, ...messages) {
-        this.assertTruthy(value !== undefined, ...messages,
-            `assertNotUndefined(${value})`);
-        return value;
-    }
-    
-    assertThrows(function_, errorType, ...messages) {
-        let return_;
-        let caught;
-        try {
-            return_ = function_();
-            caught = undefined;
-        }
-        catch (error) {
-            caught = error;
-        }
-        this.assertNotUndefined(caught, ...messages, "Threw an error.");
-        if (errorType !== undefined) {
-            if (this._popAssertion()) {
-                this.assertInstanceOf(caught, errorType, ...messages,
-                    "Threw expected error type.");
-            }
-        }
-    }
-    
-    assertTypeError(function_, ...messages) {
-        return this.assertThrows(function_, TypeError, ...messages);
     }
 
-    assertArrays(assertFunction, left, right, ...messages) {
-        this.assertEqual(left.length, right.length, ...messages, 
-            "assertArrays same length");
-        if (this._popAssertion()) {
-            for(let index=0; index < left.length; index++) {
-                const message = `assertArrays [${index}].`;
-                assertFunction.bind(this)(
-                    left[index], right[index], ...messages, message);
-                if (!this._popAssertion()) break;
-            }
-        }
+    assertNotEqual(left, right, ...messages) {
+        this._resultAssertion(
+            new TestAssertion().assertNotEqual(left, right, ...messages));
+        return [left, right];
+    }
+
+    assertTrue(value, ...messages) {
+        this._resultAssertion(
+            new TestAssertion().assertTrue(value, ...messages));
+        return value;
+    }
+
+    assertFalse(value, ...messages) {
+        this._resultAssertion(
+            new TestAssertion().assertFalse(value, ...messages));
+        return value;
+    }
+
+    assertUndefined(value, ...messages) {
+        this._resultAssertion(
+            new TestAssertion().assertUndefined(value, ...messages));
+        return value;
+    }
+
+    assertNotUndefined(value, ...messages) {
+        this._resultAssertion(
+            new TestAssertion().assertNotUndefined(value, ...messages));
+        return value;
+    }
+
+    assertThrows(function_, errorType, ...messages) {
+        const [assertion, return_] = new TestAssertion().assertThrows(
+            function_, errorType, ...messages);
+        this._resultAssertion(assertion);
+        return return_;
+    }
+
+    assertTypeError(function_, ...messages) {
+        const [assertion, return_] = new TestAssertion().assertThrows(
+            function_, TypeError, ...messages);
+        this._resultAssertion(assertion);
+        return return_;
+    }
+
+    assertEqualArrays(left, right, ...messages) {
+        this._resultAssertion(
+            new TestAssertion().assertEqualArrays(left, right, ...messages));
         return [left, right];
     }
 }
@@ -216,8 +291,8 @@ export class TestResult {
 export class TestRun {
     constructor() {
         this._results = {};
-        this.status = "TestRun tests";
-        this.runTestRunTests();
+        this.status = "Self testing";
+        this.runSelfTests();
 
         // Default values.
         this._stopOnFail = true;
@@ -245,21 +320,96 @@ export class TestRun {
     // about getter and setter inheritance:
     // https://stackoverflow.com/a/34456245/7657675
 
-    runTestRunTests() {
-        const t = new TestResult();
+    runSelfTests() {
+        // Run tests on the framework itself, using its own functionality.
+        // Create two TestResult objects so that one can make assertions about
+        // the other. Any test failures here will throw.
+        const t1 = new TestResult();
+        const t2 = new TestResult();
 
-        t.assertThrows(
-            () => t.assertFalse(true, "runTestRunTests dummy assertion."),
-            TestFailedError
+        t2.assertEqual("Self testing", this.status, "runSelfTests: status.");
+
+        t2.assertUndefined(t1.lastFail,
+            "runSelfTests: No fails before any assertions.");
+        t2.assertUndefined(t1.passed,
+            "runSelfTests: Passed undefined before any assertions.");
+        t2.assertUndefined(t1.failed,
+            "runSelfTests: Failed undefined before any assertions.");
+        t2.assertTrue(t2.passed,
+            "runSelfTests: Passed OK after three assertions");
+        t2.assertFalse(t2.failed,
+            "runSelfTests: Didn't fail after three assertions");
+
+        t1.assertTypeEqual(2, 3, "runSelfTests: assertTypeEqual.");
+        t1.assertInstanceOf(
+            t1, TestResult, "runSelfTests: assertInstanceOf.");
+
+        t2.assertUndefined(t1.lastFail,
+            "runSelfTests: No fails if all assertions have passed.");
+
+        const assertionCount = t2.assertions.length;
+        t2.assertThrows(
+            // The assertFalse here will fail and throw.
+            () => t1.assertFalse(true, "runSelfTests: Deliberate fail."),
+            TestAssertion
+        );
+        t2.assertEqual(
+            assertionCount + 1, t2.assertions.length,
+            "runSelfTests: assertThrows adds one assertion.");
+        t2.assertNotUndefined(t1.lastFail,
+            "runSelfTests: Failed assertion after assertFalse dummy.");
+
+        // Nested assertThrows. The inner should fail and throw a TestAssertion,
+        // because its function doesn't throw. The outer should catch it and
+        // pass.
+        t2.assertThrows(
+            () => {
+                t2.assertThrows(
+                    () => { let noOperation; },
+                    undefined,
+                    "runSelfTests: Deliberate lack of throw."
+                );
+            },
+            TestAssertion,
+            "runSelfTests: inner assertThrows fail caught by outer."
+        );
+
+        t1.assertThrows(
+            () => { let undefined_; const length = undefined_.length; },
+            undefined,
+            "runSelfTests assertThrows with undefined error type."
+        );
+        t1.assertTypeError(
+            () => { let undefined_; const length = undefined_.length; },
+            "runSelfTests undefined.length TypeError."
+        );
+
+        t1.assertThrows(
+            () => t1.assertNotEqual(
+                "2", 2,
+                "runSelfTests Deliberate fail different types inequality."
+            ),
+            TestAssertion
+        );
+
+        t1.assertNotEqual(3, 2, "runSelfTests assertNotEqual.");
+        t1.assertEqual(3, 3, "runSelfTests assertEqual.");
+
+        t1.assertThrows(
+            () => t1.assertEqual(
+                "2", 2,
+                "runSelfTests Deliberate fail different types equality."
+            ),
+            TestAssertion
         );
 
         const left = [1, 2];
         const right = [1, 0];
-        t.assertThrows(
-            () => t.assertArrays(t.assertEqual, left, right), TestFailedError
+        t2.assertThrows(
+            () => t1.assertEqualArrays(left, right), TestAssertion
         );
-
-        t.assertArrays(t.assertNotEqual, left, right);
+        right[1] = left[1];
+        t1.assertEqualArrays(left, right);
     }
 
     run(tests) {
@@ -281,7 +431,7 @@ export class TestRun {
             if (testResult.failed) {
                 if (testResult.throwOnFail) {
                     this.status = `Thrown out by: ${name}`;
-                    throw testResult.lastError;
+                    throw testResult.lastFail;
                 }
                 if (this.stopOnFail) {
                     stopped = true;
