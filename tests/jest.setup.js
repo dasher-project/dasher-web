288c91ae9b4ca74b0a0d3a8627badb1aaf79845e
will wade <willwade@gmail.com>
2026-02-21 16:18:28 +0000

    jest polyfill

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,3 +1,13 @@
+const util = require("node:util");
+
+if (typeof globalThis.structuredClone !== "function") {
+  if (typeof util.structuredClone === "function") {
+    globalThis.structuredClone = util.structuredClone;
+  } else {
+    globalThis.structuredClone = (value) => JSON.parse(JSON.stringify(value));
+  }
+}
+
 global.console = {
   log: jest.fn(),
   debug: console.debug,
